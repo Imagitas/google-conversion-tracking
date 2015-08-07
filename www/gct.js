000dff06dc3330941cf0a0eb5b0337ce16563229
Jason Davis <jbdavis80@gmail.com>
2015-08-07 17:34:33 -0400

    version 0.0.4 - fix js helper

--- www/gct.js
+++ www/gct.js
@@ -1,15 +1,11 @@
 var GoogleConversionTracking = (function () {
     var GoogleConversionTracking = (typeof exports !== 'undefined') && exports || {};
 
-    GoogleConversionTracking.doNothing = function () {
-    };
-
     var PLUGIN_NAME = "GoogleConversionTracking";
 
     GoogleConversionTracking.track = function (conversionId, label, value, repeatable, success, fail) {
-        exec(success, fail, 'GoogleConversionTracking', 'track', [conversionId, label, value, repeatable]);
+        return cordova.exec(success, fail, PLUGIN_NAME, 'track', [conversionId, label, value, repeatable]);
     };
-
 
     return GoogleConversionTracking;
 }());
